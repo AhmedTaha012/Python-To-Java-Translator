7d4f131334f9b517b726a15ac770fa2c0348cafb
AhmedTaha <96799091+AhmedTaha012@users.noreply.github.com>
2023-05-17 20:32:10 +0300

    javatempfile

---
+++ temp.java
@@ -0,0 +1,17 @@
+class Test {public static void main(String[] args) { int x=10;
+for(int i=0;i<10;i++){
+    while((x!=3)||(x<5)){
+        if(x%2==0){
+            System.out.print("even");
+        }
+        else if(x==2){
+           System.out.print("equal"); 
+        }
+        else{
+            System.out.print("odd");
+        }
+    }
+}
+
+    
+}}
